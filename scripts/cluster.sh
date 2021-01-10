ce6562b607103e2ee25e89cf153ae9c328603e5c
Spyros Briakos <spympr.6@gmail.com>
2021-01-10 11:50:30 +0200

    update

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -1,5 +1,3 @@
 make cluster 
-
-# time ./cluster -d data/train/train-images-idx3-ubyte -i data/new/newtrain-images-idx3-ubyte -n data/clusterfile.txt -c data/cluster.conf -o output
-time ./cluster -d data/test/t10k-images-idx3-ubyte -i data/new/newt10k-images-idx3-ubyte -n data/clusterfile.txt -c data/cluster.conf -o output
-# time valgrind --leak-check=full ./cluster –d <input file original space> -i <input file new space> -n <classes from NN as clusters file> –c <configuration file> -o <output file>
+time ./cluster -d data/test/t10k-images-idx3-ubyte -i data/new/newt10k-images-idx3-ubyte -n data/clusterfile.txt -c data/cluster.conf -o output # Input: 10.000 images
+# time ./cluster -d data/train/train-images-idx3-ubyte -i data/new/newtrain-images-idx3-ubyte -n data/clusterfile.txt -c data/cluster.conf -o output # Input: 60.000 images
